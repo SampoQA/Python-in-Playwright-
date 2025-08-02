6bfcd67ce6ecdb8558a3c6c9883598d8fff240eb
SampoQA <tertusampo@gmail.com>
2025-08-02 13:48:46 +0300

    Update test_titles.py

--- test_titles.py
+++ test_titles.py
@@ -7,4 +7,17 @@ def test_main_page_title(page: Page):
     # Для надежности используйте частичную проверку
     title = page.title()
     assert "Литрес" in title
+
     assert "электронных и аудиокниг" in title
+
+def test_audiobook_page_title(page: Page):
+    page.goto("https://www.litres.ru/")
+    page.locator("xpath=//*[@id=\"lowerMenuWrap\"]/nav/div/a[6]").click()
+    
+    # Выводим фактический title перед проверкой
+    actual_title = page.title()
+    print(f"\nФактический title страницы: '{actual_title}'")
+    
+    expect(page).to_have_title("Аудиокниги – слушать онлайн или скачать в mp3 на Литрес")
+    page.wait_for_timeout(3000)
+   
